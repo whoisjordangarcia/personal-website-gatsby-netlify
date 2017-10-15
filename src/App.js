9ed66867c64e2b87ea5c84a429c56b8748d3fae4
Jordan Garcia <arickho@gmail.com>
2017-10-15 01:59:13 -0400

    Start creation of new version of personal website

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { SocialIcon } from "react-social-icons";
+import Typing, { Backspace, Cursor, Delay, Reset, Speed } from "../src";
+
+import logo from "./logo.svg";
+import profilePhoto from "./profile_photo.jpg";
+
+import "./App.css";
+
+import Wave from "./components/wave";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="personal-profile">
+          <img className="profile-photo" src={profilePhoto} />
+          <div className="profile-info">
+            <h1>
+              Hello! <Wave />
+            </h1>
+
+            <h2>
+              I'm <span className="full-name">Jordan Garcia</span>, a Senior
+              Software Engineer at {" "}
+              <a href="https://tigerspike.com/" target="_blank">
+                Tigerspike
+              </a>. Currently residing in New York, New York
+            </h2>
+            <div className="social-container">
+              <SocialIcon
+                network="github"
+                url="https://github.com/arickho"
+                className="social-icon"
+                color="#e7e7e7"
+              />
+              <SocialIcon
+                network="email"
+                url="mailto:arickho@gmail.com"
+                className="social-icon"
+                color="#e7e7e7"
+              />
+              <SocialIcon
+                network="linkedin"
+                url="https://linkedin.com/in/arickhogarcia"
+                className="social-icon"
+                color="#e7e7e7"
+              />
+              <SocialIcon
+                network="instagram"
+                url="https://www.instagram.com/whoisjordangarcia/"
+                className="social-icon"
+                color="#e7e7e7"
+              />
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/wave/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./style.css";
+
+const wave = () => <span className="wave animated"> </span>;
+
+export default wave;

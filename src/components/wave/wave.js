4db906cfa18f410f6ce0cac94c5ef6704f2206a8
Jordan Garcia <arickho@gmail.com>
2018-04-17 21:50:22 -0400

    new yarn lock and updated wave files

--- src/components/wave/wave.js
+++ src/components/wave/wave.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
-import waveImage from './wave.png';
+import waveImage from './Wave.png';
 
 const waveAnimation = keyframes`
   from {

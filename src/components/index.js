b78fe86a338ffd4b67755072a636529fb3d79a27
Jordan Garcia <arickho@gmail.com>
2018-04-29 20:20:43 -0400

    Export fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as Wave } from './Wave/Wave';
+export Wave from './Wave/Wave';
 export { Heading, Link, Paragraph } from './Elements';
 export { default as MovingBlob } from './MovingBlob/MovingBlob';

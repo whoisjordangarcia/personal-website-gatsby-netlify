cac0637fcc85cb58ec4f595edc587737c74c9412
Jordan Garcia <arickho@gmail.com>
2018-10-17 18:23:56 +0100

    Update component reference

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export {default as Wave} from './Wave';
-export { Heading, Link, Paragraph } from './Elements';
 export { default as MovingBlob } from './MovingBlob/MovingBlob';
+export { default as Wave } from './Wave';
+export { Heading, Link, Paragraph } from './Elements';

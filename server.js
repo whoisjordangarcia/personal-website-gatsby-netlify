11451690230dbdcb9e39a16a6670e9a1c80025d5
Jordan Garcia <arickho@gmail.com>
2019-07-30 20:07:56 -0400

    add production env

--- server.js
+++ server.js
@@ -1,64 +1,64 @@
-const express = require('express')
-const next = require('next')
-const { parse } = require('url')
+const express = require('express');
+const next = require('next');
+const { parse } = require('url');
 
-const isProd = process.env.NODE_ENV === 'production'
-const isStaging = process.env.NODE_ENV === 'staging'
+const isProd = process.env.NODE_ENV === 'production';
+const isStaging = process.env.NODE_ENV === 'staging';
 
-const dev = !isProd || !isStaging
+const dev = !isProd || !isStaging;
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const app = next({ dir: '.', dev })
-const handle = app.getRequestHandler()
+const app = next({ dir: '.', dev });
+const handle = app.getRequestHandler();
 
-const getRoutes = require('./routes')
+const getRoutes = require('./routes');
 
-const routes = getRoutes()
+const routes = getRoutes();
 
 app.prepare().then(() => {
-    const server = express()
+  const server = express();
 
-    server.get('/robots.txt', (req, res) =>
-        res.status(200).sendFile('robots.txt', {
-            root: __dirname + '/static/',
-            headers: {
-                'Content-Type': 'text/plain;charset=UTF-8',
-            },
-        })
-    )
-
-    server.get('/sitemap.xml', (req, res) =>
-        res.status(200).sendFile('sitemap.xml', {
-            root: __dirname + '/static/',
-            headers: {
-                'Content-Type': 'text/xml;charset=UTF-8',
-            },
-        })
-    )
-
-    server.get('*', (req, res) => {
-        const parsedUrl = parse(req.url, true)
-        const { pathname, query = {} } = parsedUrl
-        const route = routes[pathname]
-
-        let assetPrefix = ''
-        if (isProd) {
-            assetPrefix = 'https://static.jordangarcia.me'
-        } else if (isStaging) {
-            assetPrefix = 'https://de54hm83ewhv0.cloudfront.net'
-        }
-
-        app.setAssetPrefix(assetPrefix)
-
-        if (route) {
-            return app.render(req, res, route.page, query)
-        }
-        return handle(req, res)
+  server.get('/robots.txt', (req, res) =>
+    res.status(200).sendFile('robots.txt', {
+      root: __dirname + '/static/',
+      headers: {
+        'Content-Type': 'text/plain;charset=UTF-8'
+      }
     })
+  );
 
-    server.listen(PORT, err => {
-        if (err) throw err
-        console.log(`> Ready on http://localhost:${PORT}`)
+  server.get('/sitemap.xml', (req, res) =>
+    res.status(200).sendFile('sitemap.xml', {
+      root: __dirname + '/static/',
+      headers: {
+        'Content-Type': 'text/xml;charset=UTF-8'
+      }
     })
-})
+  );
+
+  server.get('*', (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname, query = {} } = parsedUrl;
+    const route = routes[pathname];
+
+    let assetPrefix = '';
+    if (isProd) {
+      assetPrefix = 'https://static.jordangarcia.me';
+    } else if (isStaging) {
+      assetPrefix = 'https://de54hm83ewhv0.cloudfront.net';
+    }
+
+    app.setAssetPrefix(assetPrefix);
+
+    if (route) {
+      return app.render(req, res, route.page, query);
+    }
+    return handle(req, res);
+  });
+
+  server.listen(PORT, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${PORT}`);
+  });
+});

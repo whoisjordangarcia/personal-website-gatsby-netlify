c04b6a84b1df935845fa7896bdbaf36764d82dc9
Jordan Garcia <arickho@gmail.com>
2019-08-30 17:43:42 -0400

    resolve assert prefix

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const next = require('next');
 const { parse } = require('url');
 
 const isProd = process.env.NODE_ENV === 'production';
+<<<<<<< Updated upstream
 const isStaging = process.env.NODE_ENV === 'staging';
 
 const dev = !isProd || !isStaging;
@@ -14,6 +15,15 @@ const handle = app.getRequestHandler();
 
 const getRoutes = require('./routes');
 
+=======
+const PORT = process.env.PORT || 3000;
+
+const app = next({ dir: '.', dev: !isProd });
+const handle = app.getRequestHandler();
+
+const getRoutes = require('./routes');
+
+>>>>>>> Stashed changes
 const routes = getRoutes();
 
 app.prepare().then(() => {
@@ -42,6 +52,7 @@ app.prepare().then(() => {
     const { pathname, query = {} } = parsedUrl;
     const route = routes[pathname];
 
+<<<<<<< Updated upstream
     let assetPrefix = '';
     if (isProd) {
       assetPrefix = 'https://static.jordangarcia.me';
@@ -49,10 +60,19 @@ app.prepare().then(() => {
       assetPrefix = 'https://de54hm83ewhv0.cloudfront.net';
     }
     app.setAssetPrefix(assetPrefix);
+=======
+    if (process.env.ASSET_PREFIX) {
+      app.setAssetPrefix(process.env.ASSET_PREFIX);
+    }
+>>>>>>> Stashed changes
 
     if (route) {
       return app.render(req, res, route.page, query);
     }
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     return handle(req, res);
   });
 

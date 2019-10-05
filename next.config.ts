13dbfe45cc2f998b8b4d5809e06c4452bfc61e80
Jordan Garcia <arickho@gmail.com>
2019-10-05 18:07:40 -0400

    remove next images

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
-import withImages from 'next-images';
+// import withImages from 'next-images';
 
 import { routes } from './routes';
 
-module.exports = withImages({
+// module.exports = withImages({
+//   exportPathMap: routes,
+//   useFileSystemPublicRoutes: false
+// });
+
+module.exports = {
   exportPathMap: routes,
   useFileSystemPublicRoutes: false
-});
+};

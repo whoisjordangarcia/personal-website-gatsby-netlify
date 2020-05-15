8a6a479f4ea6ff66dcf0590dc13e117acef5eca2
Jordan Garcia <jordan.garcia@invitae.com>
2020-05-15 17:41:56 -0400

    remove comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,46 +13,14 @@ module.exports = {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
         id: 'UA-86912357-1',
-
-        // Include GTM in development.
-        // Defaults to false meaning GTM will only be loaded in production.
         includeInDevelopment: true,
-
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        // Defaults to null
         defaultDataLayer: { platform: 'gatsby' },
-
-        // Specify optional GTM environment details.
-        //gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
-        //gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
-        //dataLayerName: 'YOUR_DATA_LAYER_NAME'
       },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-86912357-1',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        //optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-        // Enables Google Optimize Experiment ID
-        //experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
-        // Set Variation ID. 0 for original 1,2,3....
-        //variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
-        // Any additional optional fields
-        //sampleRate: 5,
-        //siteSpeedSampleRate: 10,
-        //cookieDomain: 'example.com'
       },
     },
     `gatsby-plugin-react-helmet`,
@@ -111,17 +79,12 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        // CommonMark mode (default: true)
         commonmark: true,
-        // Footnotes mode (default: true)
         footnotes: true,
-        // Pedantic mode (default: true)
         pedantic: true,
-        // GitHub Flavored Markdown mode (default: true)
         gfm: true,
         plugins: [
           {
-            // https://www.gatsbyjs.org/packages/gatsby-remark-external-links
             resolve: 'gatsby-remark-external-links',
             options: {
               target: '_blank',

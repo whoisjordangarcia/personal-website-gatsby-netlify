cd615165f567ad199b98c68ae3c800102f2eb186
Jordan Garcia <jordan.garcia@invitae.com>
2020-05-15 15:52:00 -0400

    remove object

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  'lint-staged': {
-    'src/**/*.{ts,tsx}': ['eslint --fix', 'git add'],
-    'src/**/*.scss': ['stylelint --syntax scss --fix', 'git add'],
-  },
+  'src/**/*.{ts,tsx}': ['eslint --fix', 'git add'],
+  'src/**/*.scss': ['stylelint --syntax scss --fix', 'git add'],
 }

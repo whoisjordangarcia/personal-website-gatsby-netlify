b5c1aab18ae580f1a89579c39aa2f344764ccecf
Jordan Garcia <jordan.garcia@invitae.com>
2020-05-15 17:28:15 -0400

    add external links to markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,5 +101,35 @@ module.exports = {
         extensions: ['tsx', 'ts', 'css', 'jpg', 'png', 'jpeg'],
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        plugins: [
+          {
+            // https://www.gatsbyjs.org/packages/gatsby-remark-external-links
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'nofollow noopener noreferrer',
+            },
+          },
+        ],
+      },
+    },
   ],
 }

0023d5c0f24f58aa07a0004691a06fbf45f77a0a
Jordan Garcia <arickho@gmail.com>
2020-07-25 19:39:11 +0000

    use emotion

---
+++ src/components/ExperienceList/ExperienceList.style.ts
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled'
+
+export const Logo = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-right: 20px;
+  margin-top: 10px;
+`
+
+export const EmptyLogo = styled.div`
+  width: 30px;
+  height: 30px;
+  margin-right: 20px;
+  margin-top: 10px;
+`
+
+export const ExperienceItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+`
+
+export const Company = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
+
+export const CompanyHeading = styled.h3`
+  line-height: 45px;
+  font-size: 18px;
+  font-weight: 500;
+  margin: 0;
+  padding: 0;
+`
+
+export const Role = styled.p`
+  font-size: 18px;
+  line-height: 25px;
+  font-weight: 300;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+`
+
+export const Duration = styled.span`
+  text-align: right;
+  float: right;
+  font-size: 14px;
+  line-height: 25px;
+  font-weight: 300;
+`
--- src/components/ExperienceList/ExperienceList.tsx
+++ src/components/ExperienceList/ExperienceList.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
-import styles from './ExperienceList.module.css'
+import {
+  Logo,
+  EmptyLogo,
+  Company,
+  ExperienceItem,
+  CompanyHeading,
+  Role,
+  Duration,
+} from './ExperienceList.style'
 
 interface Experience {
   companyName: string
@@ -18,33 +26,27 @@ interface Props {
   experiences: Experience[]
 }
 
-const renderItems = (experiences: Experience[]): React.ReactNode =>
-  experiences.map((item) => (
-    <div key={item.companyName} className={styles.experience}>
-      {item.logo ? (
-        <img
-          loading="lazy"
-          alt={item.logo.alt}
-          src={item.logo.image}
-          className={styles.logo}
-        />
-      ) : (
-        <div className={styles.logo}></div>
-      )}
-      <div className={styles.company}>
-        <h3 className={styles.companyName}>{item.companyName}</h3>
-        {item.roles.map((role) => (
-          <p key={role.title} className={styles.role}>
-            {role.title}
-            <span>{role.duration}</span>
-          </p>
-        ))}
-      </div>
-    </div>
-  ))
-
 const ExperienceList: React.FC<Props> = ({ experiences }) => (
-  <div>{renderItems(experiences)}</div>
+  <div>
+    {experiences.map((item) => (
+      <ExperienceItem key={item.companyName}>
+        {item.logo ? (
+          <Logo loading="lazy" alt={item.logo.alt} src={item.logo.image} />
+        ) : (
+          <EmptyLogo />
+        )}
+        <Company>
+          <CompanyHeading>{item.companyName}</CompanyHeading>
+          {item.roles.map((role) => (
+            <Role key={role.title}>
+              {role.title}
+              <Duration>{role.duration}</Duration>
+            </Role>
+          ))}
+        </Company>
+      </ExperienceItem>
+    ))}
+  </div>
 )
 
 export default ExperienceList

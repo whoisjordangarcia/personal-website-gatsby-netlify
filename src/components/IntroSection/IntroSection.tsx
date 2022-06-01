3dc3313ccbecacbc6d8d319c92e38d9965121a28
Jordan Garcia <arickho@gmail.com>
2022-06-01 09:31:12 -0400

    update intro

--- src/components/IntroSection/IntroSection.tsx
+++ src/components/IntroSection/IntroSection.tsx
@@ -1,33 +1,37 @@
 import React from 'react'
 import Wave from 'components/Wave/Wave'
-import MovingBlob from 'components/MovingBlob/MovingBlob'
 import TechnologistIcon from 'components/TechnologistIcon/TechnologistIcon'
 import profilePhoto from 'images/profile_photo.0983212.png'
 import * as styles from './IntroSection.module.css'
+import SplineBackground from 'components/SplineBackground/SplineBackground'
 
 const IntroSection: React.FC = () => (
   <header className={styles.wrapper}>
-    <img
-      alt="Profile"
-      src={profilePhoto}
-      className={styles.profilePhoto}
-      loading="lazy"
-    />
-    <div className={styles.introSection}>
-      <h1 className={styles.introText}>
-        Hello! <Wave />
-      </h1>
-      <h2 className={styles.introText}>
-        I&apos;m <span>Jordan Garcia</span>, a engineering lead at{' '}
-        <a href="https://invitae.com" target="_blank" rel="noopener noreferrer">
-          @invitae
-        </a>
-        . An aussie &quot;aw-see&quot; (\ä-s\) currently residing in miami.{' '}
-        <TechnologistIcon />
-      </h2>
-    </div>
-    <div className={styles.blobWrapper}>
-      <MovingBlob />
+    <SplineBackground />
+    <div className={styles.content}>
+      <img
+        alt="Profile"
+        src={profilePhoto}
+        className={styles.profilePhoto}
+        loading="lazy"
+      />
+      <div className={styles.introSection}>
+        <h1 className={styles.introText}>
+          Hello! <Wave />
+        </h1>
+        <h2 className={styles.introText}>
+          I&apos;m <span>Jordan Garcia</span>, a engineering lead at{' '}
+          <a
+            href="https://invitae.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @invitae
+          </a>
+          . An aussie &quot;aw-see&quot; (\ä-s\) currently residing in miami.{' '}
+          <TechnologistIcon />
+        </h2>
+      </div>
     </div>
   </header>
 )
---
+++ src/components/SplineBackground/SplineBackground.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import * as styles from './SplineBackground.module.css'
+
+const SplineBackground: React.FC = () => (
+  <iframe
+    className={styles.wrapper}
+    src="https://my.spline.design/lightingtoruscopy-7689081df0a67844d4b88aff6e5379da/"
+    frameBorder="0"
+    width="100%"
+    height="100%"
+  ></iframe>
+)
+
+export default SplineBackground
